146c81f7106dad27407345d1d0028ebdc5aa38b5
Sirajuddin Khan <sirajuddinkhan88@gmail.com>
2018-09-11 20:56:42 +0530

    testng-parallell-test

---
+++ src/test/java/com/lambdatest/SingleTest.java
@@ -0,0 +1,42 @@
+package com.lambdatest;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SingleTest extends LambdaTestJavaTestNG {
+
+    @Test
+    public void test() {
+		System.out.println("Loading Url");
+                driver.get("https://4dvanceboy.github.io/lambdatest/todo.html");
+
+                
+                System.out.println("Checking Box");
+                driver.findElement(By.name("todo-4")).click();
+
+                System.out.println("Checking Another Box");
+                driver.findElement(By.name("todo-5")).click();
+                
+                // If both clicks worked, then the following List should be have length 2
+                List<WebElement> elems = driver.findElements(By.className("done-true"));
+                // So we'll assert that this is correct.
+                Assert.assertEquals(2, elems.size());
+                
+                System.out.println("Entering Text");
+                driver.findElement(By.id("todotext")).sendKeys("Get Taste of Lambda and Stick to It");
+                driver.findElement(By.id("addbutton")).click();
+                
+                // Let's also assert that the todo we added is present in the list.
+                
+                String spanText = driver.findElement(By.xpath("/html/body/div/div/div/ul/li[6]/span")).getText();
+                Assert.assertEquals("Get Taste of Lambda and Stick to It", spanText);       
+                
+                System.out.println("Taking Snapshot");
+                System.out.println("TestFinished");
+	}
+    }
---
+++ src/test/java/com/lambdatest/suite/SuiteTest09.java
@@ -0,0 +1,44 @@
+package com.lambdatest.suite;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.lambdatest.LambdaTestJavaTestNG;
+
+public class SuiteTest09 extends LambdaTestJavaTestNG {
+
+    @Test
+    public void test_09() {
+		System.out.println("Loading Url");
+                driver.get("https://4dvanceboy.github.io/lambdatest/todo.html");
+
+                
+                System.out.println("Checking Box");
+                driver.findElement(By.name("todo-4")).click();
+
+                System.out.println("Checking Another Box");
+                driver.findElement(By.name("todo-5")).click();
+                
+                // If both clicks worked, then the following List should be have length 2
+                List<WebElement> elems = driver.findElements(By.className("done-true"));
+                // So we'll assert that this is correct.
+                Assert.assertEquals(2, elems.size());
+                
+                System.out.println("Entering Text");
+                driver.findElement(By.id("todotext")).sendKeys("Get Taste of Lambda and Stick to It");
+                driver.findElement(By.id("addbutton")).click();
+                
+                // Let's also assert that the todo we added is present in the list.
+                
+                String spanText = driver.findElement(By.xpath("/html/body/div/div/div/ul/li[6]/span")).getText();
+                Assert.assertEquals("Get Taste of Lambda and Stick to It", spanText);       
+                
+                System.out.println("Taking Snapshot");
+                System.out.println("TestFinished");
+	}
+    }
